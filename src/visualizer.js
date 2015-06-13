ad17274daf95955ffbd246ec22f10814c2c2330c
Ryan Cheung <cheung31@gmail.com>
2015-06-13 17:17:03 -0400

    Grow circles.

--- src/visualizer.js
+++ src/visualizer.js
@@ -29,18 +29,44 @@ Visualizer.prototype.stop = function () {
     clearInterval(this._intervalId);
 };
 
+// velocity > 0
 Visualizer.prototype.onKeyPress = function (data) {
     var color = Visualizer.getRandomColor();
     var html = keyTemplate.render({
        color: color
     }); 
     var circleKeyEl = $(html);
-    circleKeyEl.css('background-color', color);
+
+    var containerWidth = this.$el.width();
+    var containerHeight = this.$el.height();
+
+    var circleWidth = 100;
+
+    var x = Visualizer.getRandomFromRange(circleWidth, containerWidth - circleWidth);
+    var y = Visualizer.getRandomFromRange(circleWidth,  containerHeight - circleWidth);
+
+    //circleKeyEl.css('background-color', color);
+    circleKeyEl.find('.key-circle-velocity').css('background-color', color);
+    circleKeyEl.find('.key-circle-duration').css('background-color', color);
+    circleKeyEl.css('transform', 'translate('+ x +'px,' + y + 'px)');
     this.$el.append(circleKeyEl);
+    setTimeout(function () {
+        circleKeyEl.addClass('key-circle-hold');
+    }, 100);
 };
+
+Visualizer.prototype.onKeyOff = function (data) {
+}
+
+// velocity == 0
+// KeyOff
 
 Visualizer.getRandomColor = function () {
     return '#'+Math.floor(Math.random()*16777215).toString(16);
 };
+
+Visualizer.getRandomFromRange = function (min, max) {
+    return Math.random() * (max - min) + min;
+}
 
 module.exports = Visualizer;
