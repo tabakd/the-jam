facd3256728a02bebd1085052fe05d3340831c53
Daniel Tabak <daniel@tabak.ca>
2015-06-13 18:03:01 -0400

    added unique users

--- public/app.js
+++ public/app.js
@@ -26,22 +26,27 @@ var user = Math.random().toString(36).substring(7)
 
 document.addEventListener("keydown", function(e){
   socket.emit('message', {
-    user: 'Daniel',
+    user: user,
     message: [144, 44 + keyboard.indexOf(String.fromCharCode(e.keyCode).toLowerCase()), 100]
   })
 })
 
 document.addEventListener("keyup", function(e){
   socket.emit('message', {
-    user: 'Daniel',
+    user: user,
     message: [144, 44 + keyboard.indexOf(String.fromCharCode(e.keyCode).toLowerCase()), 0]
   })
 })
 
 var notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
+
+var square   = new Wad({ source : 'square' })
+
+
 socket.on('message', function (data) {
   if(data.message[2] != 0){
-    piano.play({ pitch : notes[data.message[1] % 12] + 4 })
+  square.play({pitch: notes[data.message[1] % 12]+4})
   }
 });
+
