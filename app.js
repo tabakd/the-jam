be03bf145e43c67f9af6a8c668046c1143e32a5a
Daniel Tabak <daniel@tabak.ca>
2015-06-13 14:25:34 -0400

    Added bower

--- app.js
+++ app.js
@@ -3,10 +3,28 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var midi = require('midi');
+var inquirer = require("inquirer");
+var _ = require('lodash')
 
 app.use(express.static(__dirname + '/public'));
 
 var input = new midi.input();
+
+//var inputs = _.range(input.getPortCount()).map(function(port){
+//    return input.getPortName(port)
+//})
+
+//inquirer.prompt([{
+//    input: "rawlist",
+//    message: "Choose a MIDI input:",
+//    name: "inputs",
+//    choices: inputs
+//  }], function(answers) {
+//  console.log(answers)
+//});
+
+
+
 input.openPort(1);
 input.ignoreTypes(false, false, false);
 
--- public/app.js
+++ public/app.js
@@ -1,5 +1,27 @@
 var socket = io.connect('http://localhost:3000');
+
+var piano = new Wad({
+    source : 'square', 
+    env : {
+        attack : .01, 
+        decay : .005, 
+        sustain : .2, 
+        hold : .015, 
+        release : .3
+    }, 
+    filter : {
+        type : 'lowpass', 
+        frequency : 1200, 
+        q : 8.5, 
+        env : {
+            attack : .2, 
+            frequency : 600
+        }
+    }
+})
+
 socket.on('message', function (data) {
+  piano.play({ pitch : 'C5' })
   console.log(data.message)
 });
 
