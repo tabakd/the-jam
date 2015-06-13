41defbee0022b06fd84a04744756c58a989b3588
Daniel Tabak <daniel@tabak.ca>
2015-06-13 16:29:08 -0400

    Synth changes

--- public/app.js
+++ public/app.js
@@ -1,26 +1,37 @@
-var socket = io.connect('http://localhost:3000');
+var socket = io.connect('http://' + location.host);
 
 var piano = new Wad({
     source : 'square', 
     env : {
         attack : .01, 
-        decay : .005, 
-        sustain : .2, 
-        hold : .015, 
+        decay : 1, 
+        sustain : 1.0, 
+        hold : 1.0, 
         release : .3
     }, 
-    filter : {
-        type : 'lowpass', 
-        frequency : 1200, 
-        q : 8.5, 
-        env : {
-            attack : .2, 
-            frequency : 600
-        }
-    }
+//    filter : {
+//        type : 'lowpass', 
+//        frequency : 1200, 
+//        q : 5, 
+//        env : {
+//            attack : 0, 
+//            frequency : 1200
+//        }
+//    }
 })
 
 var notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+
+var keyboard = "qawsedrftgyhujikolp"
+
+document.addEventListener("keydown", function(e){
+  console.log(String.fromCharCode(e.keyCode))
+  console.log(keyboard.indexOf(String.fromCharCode(e.keyCode).toLowerCase()))
+  socket.emit('message', {
+    user: 'Daniel',
+    message: [144, 44 + keyboard.indexOf(String.fromCharCode(e.keyCode).toLowerCase()), 100]
+  })
+})
 
 socket.on('message', function (data) {
   if(data.message[2] != 0){
