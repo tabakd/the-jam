3451ef6d318c314af10954c30e6d9c2c9eb5748c
Daniel Tabak <daniel@tabak.ca>
2015-06-13 15:41:26 -0400

    added midi controller companion client

--- app.js
+++ app.js
@@ -2,38 +2,15 @@ var express = require('express')
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var midi = require('midi');
-var inquirer = require("inquirer");
-var _ = require('lodash')
 
 app.use(express.static(__dirname + '/public'));
 
-var input = new midi.input();
-
-//var inputs = _.range(input.getPortCount()).map(function(port){
-//    return input.getPortName(port)
-//})
-
-//inquirer.prompt([{
-//    input: "rawlist",
-//    message: "Choose a MIDI input:",
-//    name: "inputs",
-//    choices: inputs
-//  }], function(answers) {
-//  console.log(answers)
-//});
-
-
-
-input.openPort(1);
-input.ignoreTypes(false, false, false);
-
 io.on('connection', function (socket) {
-  input.on('message', function(deltaTime, message) {
-    socket.emit('message', { message: message, deltaTime: deltaTime })
+  socket.on('message', function(data) {
+    console.log(data)
+    io.emit('message', data)
   });
 })
-
 
 server.listen(3000, function(){
   var host = server.address().address;
