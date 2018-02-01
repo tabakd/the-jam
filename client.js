da3280e27af06bdfdb42f4b9c04af0ab02cd55e5
Daniel Tabak <daniel@tabak.ca>
2018-02-01 00:22:46 -0500

    Update client.js

--- client.js
+++ client.js
@@ -1,33 +1,18 @@
 var socket = require('socket.io-client')('http://localhost:3000');
 var midi = require('midi');
 var inquirer = require("inquirer");
-var _ = require('lodash')
+var _ = require('lodash');
 
 var input = new midi.input();
-
-//var inputs = _.range(input.getPortCount()).map(function(port){
-//    return input.getPortName(port)
-//})
-
-//inquirer.prompt([{
-//    input: "rawlist",
-//    message: "Choose a MIDI input:",
-//    name: "inputs",
-//    choices: inputs
-//  }], function(answers) {
-//  console.log(answers)
-//});
-
-
 
 input.openPort(1);
 input.ignoreTypes(false, false, false);
 
-var user = Math.random().toString(36).substring(7)
+var user = Math.random().toString(36).substring(7);
 
 socket.on('connect', function () {
   input.on('message', function(deltaTime, message) {
     console.log(message)
-    socket.emit('message', { message: message, deltaTime: deltaTime, user: user })
+    socket.emit('message', { message: message, deltaTime: deltaTime, user: user });
   });
 })
